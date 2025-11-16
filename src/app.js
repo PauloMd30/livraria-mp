7b5080a8ce525a0364ea4f835a503b72da436ecd
paulo <paulosilvarl17@gmail.com>
2025-11-16 19:35:11 -0300

    atualizando o app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import path from "path";
+import fs from "fs";
 
 import bookRoutes from "./routes/bookRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -12,6 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/admin", migrateRoutes);
+
+const uploadsPath = path.join(process.cwd(), "uploads");
+if (!fs.existsSync(uploadsPath)) {
+  fs.mkdirSync(uploadsPath, { recursive: true });
+  console.log("Pasta uploads criada automaticamente!");
+}
+
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // Rotas
